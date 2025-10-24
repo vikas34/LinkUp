44124831ceb01c9241bae72f7b822e798d027e49
Vikas <vky2334@gmail.com>
2025-10-24 15:24:46 +0530

    Completed image, video & text story functionality with scrollingbar

--- client/src/components/StoriesBar.jsx
+++ client/src/components/StoriesBar.jsx
@@ -3,6 +3,7 @@ import { dummyStoriesData, assets } from "../assets/assets";
 import { Plus } from "lucide-react";
 import moment from "moment";
 import StoryModel from "./StoryModel";
+import StoryViewer from "./StoryViewer";
 
 const StoriesBar = () => {
   const [stories, setStories] = useState([]);
@@ -35,7 +36,7 @@ const StoriesBar = () => {
         </div>
         {/* Story Card */}
         {stories.map((story, index) => (
-          <div
+          <div onClick={()=>setViewStory(story)}
             key={index}
             className={`relative rounded-lg shadow min-w-30 max-w-30 max-h-40 cursor-pointer hover:shadow-lg 
                     transition-all duration-200 bg-gradient-to-b from-indigo-500 to-purple-600 hover:from-indigo-700 
@@ -73,6 +74,9 @@ const StoriesBar = () => {
       </div>
       {/* ---------Add Story Model----------*/}
       {showModal && <StoryModel setShowModal={setShowModal} fetchStories={fetchStories}/>}
+
+      {/*---------- View Story Model-------- */}
+      {viewStory && <StoryViewer viewStory={viewStory} setViewStory={setViewStory}/>}
     </div>
   );
 };
