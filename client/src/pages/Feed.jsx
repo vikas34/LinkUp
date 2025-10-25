eb400bad8983df21c77620d7db445e81d6c97279
Vikas <vky2334@gmail.com>
2025-10-25 17:02:34 +0530

    Completed Feed page

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -3,6 +3,7 @@ import { assets, dummyPostsData } from "../assets/assets";
 import Loading from "../components/Loading";
 import StoriesBar from "../components/StoriesBar";
 import PostCard from "../components/PostCard";
+import RecentMessages from "../components/RecentMessages";
 
 const Feed = () => {
   const [feeds, setFeeds] = useState([]);
@@ -33,12 +34,16 @@ const Feed = () => {
 
     </div>
     {/*--------- Right Sidebar----------- */}
-    <div>
-      <div>
-        <h1>Sponsored</h1>
+
+    <div className="max-xl:hidden sticky top-0">
+      <div className="max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow">
+        <h3 className="text-slate-800 font-semibold ">Sponsored</h3>
+        <img src={assets.sponsored_img} className="w-75 h-50 rounded-md" alt="" />
+        <p className="text-slate-600"> Email Markting</p>
+        <p className="text-slate-400">Supercharge your marketing with a powerful, easy-to-use platform built for results.</p>
 
       </div>
-      <h1>Recent Messages</h1>
+   <RecentMessages/>
 
     </div>
 
