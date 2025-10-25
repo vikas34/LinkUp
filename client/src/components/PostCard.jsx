ded5bc49d3e5a1f53c2ef0ee5635fdb3549e59c1
Vikas <vky2334@gmail.com>
2025-10-25 15:23:17 +0530

    Completed PostList on feed page

---
+++ client/src/components/PostCard.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { BadgeCheck, Heart, MessageCircle, Share2 } from "lucide-react";
+import moment from "moment";
+import { dummyUserData } from "../assets/assets";
+
+const PostCard = ({ post }) => {
+  const postWithHashtags = post.content.replace(
+    /(#\w+)/g,
+    '<span class = "text-indigo-600">$1</span>'
+  );
+
+  const [likes, setLikes] = useState(post.likes_count);
+  const currentUser = dummyUserData;
+
+  const handlelike = async () => {};
+  return (
+    <div className="bg-white rouded-xl p-4 shadow space-y-4 w-full max-w-2xl ">
+      {/* ---------user Info------- */}
+      <div className="inline-flex items-center gap-3 cursor-pointer ">
+        <img
+          src={post.user.profile_picture}
+          alt=""
+          className="w-10 h-10 rounded-full shadow"
+        />
+        <div>
+          <div className="flex items-center space-x-1 ">
+            <span>{post.user.full_name}</span>
+            <BadgeCheck className="w-4 h-4 text-blue-500" />
+          </div>
+          <div className="text-gray-500 text-sm">
+            @{post.user.username} . {moment(post.createdAt).fromNow()}
+          </div>
+        </div>
+      </div>
+      {/* Content */}
+      {post.content && (
+        <div
+          className="text-gray-800 text-sm whitespace-pre-line"
+          dangerouslySetInnerHTML={{ __html: postWithHashtags }}
+        />
+      )}
+
+      {/* images */}
+      <div className="grid grid-cols-2 gap-2">
+        {post.image_urls.map((img, index) => (
+          <img
+            src={img}
+            key={index}
+            className={`w-full h-48 object-cover rounded-lg ${
+              post.image_urls.length === 1 && "col-span-2 h-auto"
+            }`}
+          />
+        ))}
+      </div>
+      {/* Actions */}
+      <div className="flex items-center gap-4 text-gray-600 text-sm pt-2 border-t border-gray-300">
+        <div className="flex items-center gap-1">
+          <Heart
+            className={`w-4 h-4 cursor-pointer ${
+              likes.includes(currentUser._id) && "text-red-500 fill-red-500"
+            }`}
+            onClick={handlelike}
+          />
+          <span>{likes.length}</span>
+        </div>
+        <div className="flex items-center gap-1">
+          <MessageCircle className="w-4 h-4" />
+          <span>{12}</span>
+        </div>
+        <div className="flex items-center gap-1">
+          <Share2 className="w-4 h-4" />
+          <span>{7}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { assets, dummyPostsData } from "../assets/assets";
 import Loading from "../components/Loading";
 import StoriesBar from "../components/StoriesBar";
+import PostCard from "../components/PostCard";
 
 const Feed = () => {
   const [feeds, setFeeds] = useState([]);
@@ -23,7 +24,9 @@ const Feed = () => {
      
      <StoriesBar/>
       <div className="p-4 space-y-6">
-        List of Post
+      {feeds.map((post)=>(
+        <PostCard key={post._id} post={post}/>
+      ))}
 
       </div>
 
